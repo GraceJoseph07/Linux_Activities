181f5a0636de224666171cec8ebc213d27de5d56
Grace Joseph <43978741+GraceJoseph07@users.noreply.github.com>
2021-03-06 08:50:08 +0530

    Update writefifo.c

--- Day5_05.03.2021/Individual_Activity/chat_fifo/writefifo.c
+++ Day5_05.03.2021/Individual_Activity/chat_fifo/writefifo.c
@@ -1,25 +1,16 @@
- #include <stdio.h> 
-#include <string.h> 
-#include <unistd.h>
-#include <fcntl.h> 
-#include <sys/stat.h> 
-#include <sys/types.h> 
- 
- int main(){ 
-	int fd;  
-	char * fifo1 = "/tmp/fifo1";  
-	mkfifo(fifo1, 0666); 
-	char arr1[100], arr2[100]; 
-	while (1) 
-	{ 
-		fd = open(fifo1, O_WRONLY); 
-		fgets(arr2, 100, stdin); 
-		write(fd, arr2, strlen(arr2)+1); 
-		close(fd); 
-		fd = open(fifo1, O_RDONLY);  
-		read(fd, arr1, sizeof(arr1)); 
-		printf("Text received by receiver: %s\n", arr1); 
-		close(fd); 
-	} 
-	return 0; 
-} 
+#include"chat.h"
+void main() 
+{
+    int fd;
+    fd = mkfifo("pipe1",0666);
+    if(fd<0)
+        printf("\nError in creation of Pipe1");
+    else
+        printf("\nPipe1 created successfully");
+    int fd1;
+    fd1 = mkfifo("pipe2",0666);
+    if(fd1<0)
+        printf("\nError in creation of Pipe2");
+    else
+        printf("\nPipe2 created successfully\n");
+}
